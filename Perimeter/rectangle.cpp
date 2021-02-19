7e1c387838181cc57a121c4380c2da2c02468c0f
Clemente Solorio <37494038+clxmente@users.noreply.github.com>
2021-02-18 17:43:16 -0800

    licensing text

--- Perimeter/rectangle.cpp
+++ Perimeter/rectangle.cpp
@@ -1,3 +1,15 @@
+//****************************************************************************************************************************
+//Program name: "Perimeter".  This program calculates the perimeter and average side of a rectangle using user inputted      *
+//float numbers. Copyright (C) 2021 Clemente Solorio                                                                         *
+//                                                                                                                           *
+//This file is part of the software program "Perimeter".                                                                     *
+//Perimeter is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License     *
+//version 3 as published by the Free Software Foundation.                                                                    *
+//Perimeter is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied            *
+//warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.     *
+//A copy of the GNU General Public License v3 is available here:  <https:;www.gnu.org/licenses/>.                            *
+//****************************************************************************************************************************
+
 //This file
 //   File name: rectangle.cpp
 //   Language: C++
